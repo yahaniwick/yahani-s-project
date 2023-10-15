c726f6ce4e083a82bd0c01d649334e54ab0eb525
Chamathka <chamathka15644@gmail.com>
2023-10-15 18:27:31 +0530

    Code to get sum from two numbers

---
+++ Chamathka.py
@@ -0,0 +1,9 @@
+# Get the two numbers as input.
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum of the two numbers.
+sum = num1 + num2
+
+# Print the sum of the two numbers.
+print("The sum is:", sum)
